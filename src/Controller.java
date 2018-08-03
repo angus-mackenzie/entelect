6e64f51888fec6814338e39cd387686f7760960d
Joshua Benjamin <joshbenjamin13@gmail.com>
2018-08-03 19:40:56 +0200

    Added a controller

---
+++ src/Controller.java
@@ -0,0 +1,14 @@
+public class Controller{
+	
+	public Controller(){
+
+	}
+
+	public int calculateDistance (Point origin, Point dest){
+
+        int dist = (int)(Math.abs(origin.getX() - dest.getX()) + Math.abs(origin.getY() - dest.getY()));
+        return dist;
+
+    }
+
+}
