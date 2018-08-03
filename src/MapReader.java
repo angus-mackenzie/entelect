c6c6dba9f4b68f4c325b9f2d2e4b2a28cbc34aca
AngusTheMack <angusmackenzie111@gmail.com>
2018-08-03 18:24:23 +0200

    Added mines and depots arraylist

--- src/MapReader.java
+++ src/MapReader.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.*;
 /**
@@ -5,25 +6,55 @@ import java.io.*;
  * Allocating coordinates to each char in the map
  */
 public class MapReader{
-    int numberOfLines = 0;
+    int dimensions = 0;
     int numberOfWorkers = 0;
+    String map ="";
+    boolean first = True;
     /**
      * @param String the filename of the map
      */
+    ArrayList<String> lines = new ArrayList<string>();
+    ArrayList<Integer,Integer> mines = new ArrayList<Integer,Integer>();
+    ArrayList<Integer,Integer> depots = new ArrayList<Integer,Integer>();
     public MapReader(String mapFileName) throws Exception{
-        Scanner mapReader = new Scanner(new File(mapFileName));
-        while(mapReader.hasNext()){
-            if(numberOfLines==0){
+        Scanner mapScanner = new Scanner(new File(mapFileName));
+        while(mapScanner.hasNext()){
+            if(first){
                 //first line
-                numberOfWorkers = mapReader.nextInt();
+                numberOfWorkers = mapScanner.nextInt();
+                first = False;
+            }else{
+                lines.add(mapScanner.nextLine());
+                dimensions++;
+                
             }
-            //otherwise we read the line in
-            Scanner lineReader = new Scanner(mapReader.nextLine());
-            while(lineReader.next()){
-                System.out.println("This is a char: "+lineReader.next());
-            }
-            
-            numberOfLines++;
         }
+
+        for(int i = 0; i < lines.size(); i++){
+            char[] line = lines.get(i).toCharArray();
+            for(int j = 0; j < line.length;j++){
+                if(Character.isUpperCase(line[j])){
+                    //mines
+                    mines.add(i,j);
+                }else if(Character.isLowerCase(line[j])){
+                    depots.add(i,j);
+                }
+            }
+
+        }
+    }
+
+    public int getSize(){
+        return dimensions;
+    }
+    public ArrayList<Integer,Integer> getMines(){
+        return mines;
+    }
+
+    public ArrayList<Integer,Integer> getDepots(){
+        return depots;
+    }
+    public int getNumberOfWorkers(){
+        return numberOfWorkers;
     }
 }
