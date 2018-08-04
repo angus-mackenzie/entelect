3f4487d579fefc6464b9a65ebecca215703dd4f6
Joshua Benjamin <joshbenjamin13@gmail.com>
2018-08-04 09:29:28 +0200

    Changed the worker

--- src/Worker.java
+++ src/Worker.java
@@ -1,14 +1,24 @@
+
+import java.util.ArrayList;
+
+
 public class Worker{
     
     int distanceTravelled;
     Point currentPoint;
     String pathTravelled;
     String workerName ="";
-    public Worker(String name, Point start){
+    int capacity;
+    ArrayList<String> elementsCarrying;
+
+    public Worker(String name, Point start, int carryCapacity){
         workerName = name;
         distanceTravelled = 0;
         currentPoint = start;
         pathTravelled = "";
+        capacity = carryCapacity;
+
+        elementsCarrying = new ArrayList<String>();
     }
 
     public void moveToPoint(Point dest){
@@ -21,6 +31,18 @@ public class Worker{
     public int calculateDistance (Point origin, Point dest){
         int dist = (int)(Math.abs(origin.getX() - dest.getX()) + Math.abs(origin.getY() - dest.getY()));
         return dist;
+    }
+
+    public void addElement(String tag){
+        elementsCarrying.add(tag);
+    }
+
+    public void removeElement(String tag){
+        elementsCarrying.remove(tag);
+    }
+    
+    public boolean hasElement(String tag){
+        return elementsCarrying.contains(tag);
     }
     
     public String printPath(){
