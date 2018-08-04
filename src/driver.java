5f6d6824cd42923d2abf583ecba9b222e3548287
Angus Mackenzie <angusmackenzie111@gmail.com>
2018-08-04 10:36:58 +0200

    added framework for fergus code'

--- src/driver.java
+++ src/driver.java
@@ -4,8 +4,18 @@ public class driver{
     public static void main(String[] args){
         try{
             MapReader mr = new MapReader("maps/map_1.input");
-            System.out.println(mr.excavatorCount);
-
+            while(mr.)
+            for(Worker worker: mr.workers){
+                if(worker.path==0) {
+                    Factory fact = nearestValue(worker, mr.minesList,mr.factoryList);
+                    worker.moveToFactory(fact);
+                }else if(worker.path==1){
+                    worker.path--;
+                    if(worker.currentFactory){
+                        worker.
+                    }
+                }
+            }
         }catch(Exception e){
             System.out.println("No file");
             e.printStackTrace();
